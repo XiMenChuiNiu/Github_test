b319bfca657024614a338e5235f1d536bccabe98
XiMenChuiNiu <2435612739@qq.com>
2016-10-14 11:59:46 +0800

    改变颜色1
    青色

---
+++ Github_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github_test
+//
+//  Created by imac on 16/10/14.
+//  Copyright © 2016年 imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Github_test/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Github_test
+//
+//  Created by imac on 16/10/14.
+//  Copyright © 2016年 imac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
